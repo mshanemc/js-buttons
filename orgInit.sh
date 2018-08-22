d6d2bd01ccbc25dddd903a63da939259e3dfd6e9
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-22 16:13:18 -0500

    username/password generation

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+# sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json -u js -o work.shop
 sfdx force:source:push
+sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
 sfdx force:org:open
