c4a0e806e51bca1f16f7e00e13de4f462ed4b8f4
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-09 09:57:17 -0600

    no -u allowed on shane:org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json -u js -o work.shop
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json --userprefix js -o work.shop
 sfdx force:source:push
 sfdx force:mdapi:deploy -d destroy/ -w 20
 sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
